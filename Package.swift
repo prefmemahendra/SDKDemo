f63c9d31b2fa987df323d244931507fa69315f21
Mahendra Rajput <mahendra.rajput@prefme.com>
2021-10-04 11:50:50 +0530

    update
    update

--- Package.swift
+++ Package.swift
@@ -11,16 +11,17 @@ let package = Package(
             name: "SDKDemo",
             targets: ["SDKDemo"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/prefmemahendra/SDKDemo.git", from: "3.0.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//         .package(url: "https://github.com/prefmemahendra/SDKDemo.git", from: "3.0.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDKDemo",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SDKDemoTests",
             dependencies: ["SDKDemo"]),
